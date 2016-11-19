ba1e65b855f2a7b3778f85c9165f8fb784c8fc6d
wym <752140387@qq.com>
2016-11-19 10:39:03 +0800

    王一鸣创建类

--- app/src/main/java/lanou/grouptestdemo/MainActivity.java
+++ app/src/main/java/lanou/grouptestdemo/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //我写了
+        // 王一鸣写了代码
     }
 }
