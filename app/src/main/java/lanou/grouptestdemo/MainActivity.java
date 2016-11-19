777a6fab1eecc60193fe9910161ca4295e1fa8c0
麦建东 <773650919@qq.com>
2016-11-19 14:22:34 +0800

    二次

--- app/src/main/java/lanou/grouptestdemo/MainActivity.java
+++ app/src/main/java/lanou/grouptestdemo/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //我写了
         // 王一鸣写了代码
-        // 下午14:16
+        // 下午14:22
     }
 }
